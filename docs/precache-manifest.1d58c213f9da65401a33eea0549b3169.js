34a9004500806f25d76a6625e55d8e6ab3261c22
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2019-05-02 14:47:30 +0530

    Test Deploy

---
+++ docs/precache-manifest.1d58c213f9da65401a33eea0549b3169.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ffe36df33a462fd6120df010c13045",
+    "url": "/React-POC/index.html"
+  },
+  {
+    "revision": "9dbe70859deb82b81c13",
+    "url": "/React-POC/static/css/main.c604a52f.chunk.css"
+  },
+  {
+    "revision": "9affbd6b2a8b45346067",
+    "url": "/React-POC/static/js/2.86274400.chunk.js"
+  },
+  {
+    "revision": "9dbe70859deb82b81c13",
+    "url": "/React-POC/static/js/main.dae6668a.chunk.js"
+  },
+  {
+    "revision": "d43d2f2d8b364aaf02b6",
+    "url": "/React-POC/static/js/runtime~main.b7755646.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65a0adbac660e862896ecc60ea16bb7a.js"
+  "/React-POC/precache-manifest.1d58c213f9da65401a33eea0549b3169.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-POC/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
